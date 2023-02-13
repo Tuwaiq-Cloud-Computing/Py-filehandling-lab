7e9cd2c3023fea2f6694c252276db2f0bf13be01
SultanAlhajri <122810344+SultanAlhajri@users.noreply.github.com>
2023-02-13 19:19:02 +0300

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,12 @@
+file1 = open("string.txt", "r")
+filedata= file1.read()
+lines=filedata.split(",")
+firstline=lines[0]
+secondline=lines[1]
+print(firstline)
+FirstFile= open("FirstLine.txt" , "w")
+FirstFile.write(firstline)
+FirstFile.close()
+SecondFile= open("SecondLine.txt" , "w")
+SecondFile.write(secondline)
+
